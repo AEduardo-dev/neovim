7cb0cf3463f1c83ce673eb4e83fb577c1e5ecdf4
Angel Vega <angel@auterion.com>
2025-05-15 13:53:10 +0200

    add frontend fork for nvim config

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+require('config.EcoVim')
+
+require('utils.globals')
+require('utils.functions')
+
+require('config.options')
+require('config.lazy')
+require('config.keymappings')
+require('config.autocmds')
+
+require('internal.cursorword')

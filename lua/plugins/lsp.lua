000cf6814821bd63a47bd757692d931361596952
Angel Vega <angel@auterion.com>
2025-05-16 11:56:16 +0200

    Add full  frontend compatible setup for ts and js development

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,96 +1,35 @@
----@diagnostic disable: missing-fields
 return {
-  -- Customize LSP
   {
     "neovim/nvim-lspconfig",
-    -- Add, change or remove keymaps
-    init = function()
-      -- disable lsp watcher. Too slow on linux
-      local ok, wf = pcall(require, "vim.lsp._watchfiles")
-      if ok then
-        wf._watchfunc = function()
-          return function() end
-        end
-      end
-
-      --[[Modify LSP keymaps]]
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "gD", false }
-      keys[#keys + 1] = { "<leader>cl", false }
-      keys[#keys + 1] = { "<leader>cli", "<cmd>LspInfo<cr>", desc = "LspInfo" }
-      keys[#keys + 1] =
-        { "<leader>clr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", desc = "Remove workspace" }
-      keys[#keys + 1] = { "<leader>cla", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", desc = "Add workspace" }
-      keys[#keys + 1] = {
-        "<leader>cll",
-        "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>",
-        desc = "List workspace",
-      }
-    end,
-    opts = {
-      inlay_hints = {
-        enabled = false,
-      },
-      diagnostics = {
-        float = {
-          source = true,
-        },
-        virtual_text = {
-          prefix = "icons",
-          spacing = 4,
-          source = true,
-          -- source = "if_many",
-        },
-        -- virtual_text = false,
-      },
-      servers = {
-        yamlls = {},
-        -- tsserver = {
-        --   -- Need to disable this cuz `Inline Edit` won't work otherwise
-        --   single_file_support = false,
-        --   settings = {
-        --     typescript = {
-        --       inlayHints = {
-        --         includeInlayParameterNameHints = "literal",
-        --         includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        --         includeInlayFunctionParameterTypeHints = true,
-        --         includeInlayVariableTypeHints = false,
-        --         includeInlayPropertyDeclarationTypeHints = true,
-        --         includeInlayFunctionLikeReturnTypeHints = true,
-        --         includeInlayEnumMemberValueHints = true,
-        --       },
-        --     },
-        --     javascript = {
-        --       inlayHints = {
-        --         includeInlayParameterNameHints = "all",
-        --         includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-        --         includeInlayFunctionParameterTypeHints = true,
-        --         includeInlayVariableTypeHints = true,
-        --         includeInlayPropertyDeclarationTypeHints = true,
-        --         includeInlayFunctionLikeReturnTypeHints = true,
-        --         includeInlayEnumMemberValueHints = true,
-        --       },
-        --     },
-        --   },
-        -- },
-        lua_ls = {
-          settings = {
-            Lua = {
-              diagnostics = {
-                -- disable = { "missing-parameter" },
-              },
-              hint = {
-                enable = true,
-                setType = true,
-                paramType = true,
-                paramName = "All",
-                semicolon = "Disable",
-                arrayIndex = "Disable",
-              },
-            },
-          },
-        },
-      },
+    lazy = false,
+    dependencies = {
+      "mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
     },
+    config = function()
+      require('config.lsp.setup')
+      require('config.lsp.config')
+      require('config.lsp.functions')
+    end
+  },
+
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = {
+      { "<leader>cm", "<cmd>Mason<CR>", desc = "Mason" },
+    },
+  },
+
+  {
+    "antosha417/nvim-lsp-file-operations",
+    event = "LspAttach",
+    dependencies = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-tree/nvim-tree.lua" },
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end
   },
 }

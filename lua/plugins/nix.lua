6a918c7f1ea5a74f97d326f336a5f35de1d40f16
Angel Vega <angel@auterion.com>
2025-07-01 12:41:50 +0200

    chore: add diagnostics for nix language

--- lua/plugins/nix.lua
+++ lua/plugins/nix.lua
@@ -1,5 +1,15 @@
 return {
   {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, {
+        nls.builtins.code_actions.statix,
+        nls.builtins.diagnostics.statix,
+      })
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     optional = true,
     opts = {

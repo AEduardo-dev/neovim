fc0d056d6e113b581750a9d712b6716e1bca7efa
Angel Vega <angel@auterion.com>
2025-07-01 11:11:38 +0200

    chore: clean up configuration for nvim setup

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -146,23 +146,6 @@ map(
 )
 
 --     ╭───────────────────────────────────────────────────────────────────╮
---     │                         Harpoon mappings                          │
---     ╰───────────────────────────────────────────────────────────────────╯
--- stylua: ignore
-if vim.bo["filetype"] ~= "rnvimr" then
-  map("n", "<M-1>", function() require("harpoon.ui").nav_file(1) end, { desc = "Harpoon 1" })
-  map("n", "<M-2>", function() require("harpoon.ui").nav_file(2) end, { desc = "Harpoon 2" })
-  map("n", "<M-3>", function() require("harpoon.ui").nav_file(3) end, { desc = "Harpoon 3" })
-  map("n", "<M-4>", function() require("harpoon.ui").nav_file(4) end, { desc = "Harpoon 4" })
-  map("n", "<M-5>", function() require("harpoon.ui").nav_file(5) end, { desc = "Harpoon 5" })
-  map("n", "<M-6>", function() require("harpoon.ui").nav_file(6) end, { desc = "Harpoon 6" })
-  map("n", "<M-7>", function() require("harpoon.ui").nav_file(7) end, { desc = "Harpoon 7" })
-  map("n", "<M-8>", function() require("harpoon.ui").nav_file(8) end, { desc = "Harpoon 8" })
-  map("n", "<M-9>", function() require("harpoon.ui").nav_file(9) end, { desc = "Harpoon 9" })
-  map("n", "<M-0>", function() require("harpoon.ui").nav_file(0) end, { desc = "Harpoon 0" })
-end
-
---     ╭───────────────────────────────────────────────────────────────────╮
 --     │                    Visual Selection Formatting                    │
 --     ╰───────────────────────────────────────────────────────────────────╯
 map("v", "<leader>ct", "<cmd>lua vim.lsp.buf.format({async=true})<cr>", { desc = "Visual Formatting" })
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,7 +1,23 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
+    inlay_hints = {
+      enabled = false,
+    },
+    diagnostics = {
+      float = {
+        source = true,
+      },
+      virtual_text = {
+        prefix = "icons",
+        spacing = 4,
+        source = true,
+        -- source = "if_many",
+      },
+      -- virtual_text = false,
+    },
     servers = {
+      nil_ls = {},
       rust_analyzer = {
         settings = {
           ["rust-analyzer"] = {
@@ -14,6 +30,132 @@ return {
           },
         },
       },
+      yamlls = {},
+      tsserver = {
+        -- Need to disable this cuz `Inline Edit` won't work otherwise
+        single_file_support = false,
+        settings = {
+          typescript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "literal",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = false,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+          javascript = {
+            inlayHints = {
+              includeInlayParameterNameHints = "all",
+              includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+              includeInlayFunctionParameterTypeHints = true,
+              includeInlayVariableTypeHints = true,
+              includeInlayPropertyDeclarationTypeHints = true,
+              includeInlayFunctionLikeReturnTypeHints = true,
+              includeInlayEnumMemberValueHints = true,
+            },
+          },
+        },
+      },
+      lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- disable = { "missing-parameter" },
+            },
+            hint = {
+              enable = true,
+              setType = true,
+              paramType = true,
+              paramName = "All",
+              semicolon = "Disable",
+              arrayIndex = "Disable",
+            },
+          },
+        },
+      },
+      -- pyright = {},
+      pylsp = {
+        mason = false,
+        settings = {
+          pylsp = {
+            plugins = {
+              rope_autoimport = {
+                enabled = true,
+              },
+            },
+          },
+        },
+      },
+      ruff_lsp = {
+        handlers = {
+          ["textDocument/publishDiagnostics"] = function() end,
+        },
+      },
+      jedi_language_server = {},
+      tailwindcss = {
+        -- exclude a filetype from the default_config
+        filetypes_exclude = { "markdown" },
+        -- add additional filetypes to the default_config
+        filetypes_include = {},
+        -- to fully override the default_config, change the below
+        -- filetypes = {}
+      },
     },
+  },
+  setup = {
+    pylsp = function()
+      LazyVim.lsp.on_attach(function(client, _)
+        if client.name == "pylsp" then
+          -- disable hover in favor of jedi-language-server
+          client.server_capabilities.hoverProvider = false
+        end
+      end)
+    end,
+    ruff_lsp = function()
+      require("lazyvim.util").lsp.on_attach(function(client, _)
+        if client.name == "ruff_lsp" then
+          -- Disable hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end
+      end)
+    end,
+    pyright = function()
+      require("lazyvim.util").lsp.on_attach(function(client, _)
+        if client.name == "pyright" then
+          -- disable hover in favor of jedi-language-server
+          client.server_capabilities.hoverProvider = false
+        end
+      end)
+    end,
+
+    tailwindcss = function(_, opts)
+      local tw = LazyVim.lsp.get_raw_config("tailwindcss")
+      opts.filetypes = opts.filetypes or {}
+
+      -- Add default filetypes
+      vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+      -- Remove excluded filetypes
+      --- @param ft string
+      opts.filetypes = vim.tbl_filter(function(ft)
+        return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+      end, opts.filetypes)
+
+      -- Additional settings for Phoenix projects
+      opts.settings = {
+        tailwindCSS = {
+          includeLanguages = {
+            elixir = "html-eex",
+            eelixir = "html-eex",
+            heex = "html-eex",
+          },
+        },
+      }
+      -- Add additional filetypes
+      vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+    end,
   },
 }

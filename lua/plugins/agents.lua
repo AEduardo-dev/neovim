086d03b5d5a63c054d68e94286f63056ab9b97c2
AEduardo-dev <vegconed@gmail.com>
2025-07-31 12:07:13 +0200

    substitute copilot plugin with codecompanion

---
+++ lua/plugins/agents.lua
@@ -0,0 +1,117 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+      { "nvim-lua/plenary.nvim" },
+      { "render-markdown.nvim" },
+      {
+        "echasnovski/mini.diff",
+        config = function()
+          local diff = require("mini.diff")
+          diff.setup({
+            -- Disabled by default
+            source = diff.gen_source.none(),
+          })
+        end,
+      },
+      {
+        "saghen/blink.cmp",
+        lazy = false,
+        version = "*",
+        opts = {
+          keymap = {
+            preset = "enter",
+            ["<S-Tab>"] = { "select_prev", "fallback" },
+            ["<Tab>"] = { "select_next", "fallback" },
+          },
+          cmdline = { sources = { "cmdline" } },
+          sources = {
+            default = { "lsp", "path", "buffer", "codecompanion" },
+          },
+        },
+      },
+    },
+    opts = {
+      --Refer to: https://github.com/olimorris/codecompanion.nvim/blob/main/lua/codecompanion/config.lua
+      strategies = {
+        --NOTE: Change the adapter as required
+        chat = {
+          adapter = "copilot",
+          model = "claude-sonnet-4-20250514",
+          keymaps = {
+            send = {
+              modes = { n = "<C-s>", i = "<C-s>" },
+              opts = {},
+            },
+            close = {
+              modes = { n = "<C-c>", i = "<C-c>" },
+              opts = {},
+            },
+            -- Add further custom keymaps here
+          },
+          slash_commands = {
+            ["file"] = {
+              -- Location to the slash command in CodeCompanion
+              callback = "strategies.chat.slash_commands.file",
+              description = "Select a file using Telescope",
+              opts = {
+                provider = "snacks", -- Can be "default", "telescope", "fzf_lua", "mini_pick" or "snacks"
+                contains_code = true,
+              },
+            },
+          },
+          -- Change the default icons
+          icons = {
+            buffer_pin = " ",
+            buffer_watch = "👀 ",
+          },
+
+          -- Alter the sizing of the debug window
+          debug_window = {
+            ---@return number|fun(): number
+            width = vim.o.columns - 5,
+            ---@return number|fun(): number
+            height = vim.o.lines - 2,
+          },
+
+          -- Options to customize the UI of the chat buffer
+          window = {
+            layout = "vertical", -- float|vertical|horizontal|buffer
+            position = nil, -- left|right|top|bottom (nil will default depending on vim.opt.splitright|vim.opt.splitbelow)
+            border = "single",
+            height = 0.8,
+            width = 0.45,
+            relative = "editor",
+            full_height = true, -- when set to false, vsplit will be used to open the chat buffer vs. botright/topleft vsplit
+            sticky = false, -- when set to true and `layout` is not `"buffer"`, the chat buffer will remain opened when switching tabs
+            opts = {
+              breakindent = true,
+              cursorcolumn = false,
+              cursorline = false,
+              foldcolumn = "0",
+              linebreak = true,
+              list = false,
+              numberwidth = 1,
+              signcolumn = "no",
+              spell = false,
+              wrap = true,
+            },
+          },
+
+          ---Customize how tokens are displayed
+          ---@param tokens number
+          ---@param adapter CodeCompanion.Adapter
+          ---@return string
+          token_count = function(tokens, adapter)
+            return " (" .. tokens .. " tokens)"
+          end,
+        },
+        inline = { adapter = "copilot", model = "claude-sonnet-4-20250514" },
+      },
+      opts = {
+        log_level = "DEBUG",
+      },
+    },
+  },
+}

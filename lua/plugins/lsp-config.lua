1d3dc426b41afc18f5f082baa576a68ca07df568
Angel Vega <angel@auterion.com>
2025-09-19 12:57:05 +0200

    feat: removed lsp redundant python lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -95,18 +95,6 @@ return {
         },
       },
       -- pyright = {},
-      pylsp = {
-        mason = false,
-        settings = {
-          pylsp = {
-            plugins = {
-              rope_autoimport = {
-                enabled = true,
-              },
-            },
-          },
-        },
-      },
       ruff_lsp = {
         handlers = {
           ["textDocument/publishDiagnostics"] = function() end,
@@ -124,14 +112,6 @@ return {
     },
   },
   setup = {
-    pylsp = function()
-      LazyVim.lsp.on_attach(function(client, _)
-        if client.name == "pylsp" then
-          -- disable hover in favor of jedi-language-server
-          client.server_capabilities.hoverProvider = false
-        end
-      end)
-    end,
     ruff_lsp = function()
       require("lazyvim.util").lsp.on_attach(function(client, _)
         if client.name == "ruff_lsp" then
